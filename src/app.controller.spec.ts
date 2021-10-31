338c44474bfb217c1fb4b385a00e596411171161
shogo-nakano-desu <61229807+shogo-nakano-desu@users.noreply.github.com>
2021-10-31 12:48:00 +0900

    create mock

--- src/app.controller.spec.ts
+++ src/app.controller.spec.ts
@@ -1,21 +1,26 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { AppService, PublicItem } from './app.service';
 
 describe('AppController', () => {
   let appController: AppController;
+  let appService: AppService;
 
   beforeEach(async () => {
-    const app: TestingModule = await Test.createTestingModule({
-      controllers: [AppController],
-      providers: [AppService],
-    }).compile();
-
-    appController = app.get<AppController>(AppController);
+    appService = new AppService();
+    appController = new AppController(appService);
   });
 
   describe('/items', () => {
     it('should return public items', () => {
+      jest.spyOn(appService, 'getPublicItems').mockImplementation(() => {
+        const item: PublicItem = {
+          id: 1,
+          title: 'Mock Title',
+          body: 'Mock Body',
+        };
+        return [item];
+      });
       expect(appController.getItems()).toHaveLength(1);
     });
   });
